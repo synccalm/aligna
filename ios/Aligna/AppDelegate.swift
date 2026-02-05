1bc94b4d68627003d400cb60b70b4af519b21c4b
synccalm <vipin@evoqins.com>
2026-02-05 23:36:15 +0530

    ios initial commit

--- ios/Aligna/AppDelegate.swift
+++ ios/Aligna/AppDelegate.swift
@@ -1,35 +1,28 @@
 import UIKit
 import React
+import React_RCTAppDelegate
+import ReactAppDependencyProvider
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
+
+  var reactNativeDelegate: ReactNativeDelegate?
   var reactNativeFactory: RCTReactNativeFactory?
-  var reactDelegate: RCTDefaultReactNativeFactoryDelegate?
 
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
-
-    // Create the delegate
-    let delegate = RCTDefaultReactNativeFactoryDelegate()
-
-    // Create the dependency provider (no AppDependencyProvider class in RN 0.80)
-    let dependencyProvider = RCTAppDependencyProvider()
-
-    // Assign dependency provider to delegate
-    delegate.dependencyProvider = dependencyProvider
-
-    // Create the factory
+    let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
+    delegate.dependencyProvider = RCTAppDependencyProvider()
 
-    // Keep references
+    reactNativeDelegate = delegate
     reactNativeFactory = factory
-    reactDelegate = delegate
 
-    // Create and start the main window
     window = UIWindow(frame: UIScreen.main.bounds)
+
     factory.startReactNative(
       withModuleName: "Aligna",
       in: window,
@@ -39,3 +32,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     return true
   }
 }
+
+class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
+  override func sourceURL(for bridge: RCTBridge) -> URL? {
+    self.bundleURL()
+  }
+
+  override func bundleURL() -> URL? {
+#if DEBUG
+    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+#else
+    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+#endif
+  }
+}
+
